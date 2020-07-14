efed2e5e4d8c79e522a23970c17720aea25c6558
Selcuk <devonukselcuk@gmail.com>
2020-07-14 20:50:09 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`Server started on port ${PORT}`);
+});
